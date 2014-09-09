d33454eaab12c435aff60b5d3c4dd5b7f3b89a00
Kpetermeni Siakor <tksiakor@gmail.com>
2014-09-09 16:52:52 +0000

    Step 3 Done by Kpetermeni

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -4,26 +4,32 @@
 
 describe('PhoneCat App', function() {
 
-  describe('Phone list view', function() {
+    describe('Phone list view', function() {
 
-    beforeEach(function() {
-      browser.get('app/index.html');
-    });
-
-
-    it('should filter the phone list as user types into the search box', function() {
+      beforeEach(function() {
+        browser.get('app/index.html');
+      });
 
       var phoneList = element.all(by.repeater('phone in phones'));
       var query = element(by.model('query'));
 
-      expect(phoneList.count()).toBe(3);
+      it('should filter the phone list as user types into the search box', function() {
+        expect(phoneList.count()).toBe(3);
 
-      query.sendKeys('nexus');
-      expect(phoneList.count()).toBe(1);
+        query.sendKeys('nexus');
+        expect(phoneList.count()).toBe(1);
 
-      query.clear();
-      query.sendKeys('motorola');
-      expect(phoneList.count()).toBe(2);
+        query.clear();
+        query.sendKeys('motorola');
+        expect(phoneList.count()).toBe(2);
+      });
+
+      it('should display the current filter value in the title bar', function() {
+        query.clear();
+        expect(browser.getTitle()).toMatch(/Google Phone Gallery:\s*$/);
+
+        query.sendKeys('nexus');
+        expect(browser.getTitle()).toMatch(/Google Phone Gallery: nexus$/);
+      });
     });
   });
-});
