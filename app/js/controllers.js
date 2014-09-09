6e348ad0c82447f33b53d2582beeeab81cfe7891
Kpetermeni Siakor <tksiakor@gmail.com>
2014-09-09 17:29:46 +0000

    Step 5: Done by Kpetermeni

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,8 +6,11 @@ var phonecatApp = angular.module('phonecatApp', []);
 
 phonecatApp.controller('PhoneListCtrl', function($scope, $http) {
   $http.get('phones/phones.json').success(function(data) {
-    $scope.phones = data;
+    //$scope.phones = data;
+    $scope.phones = data.splice(0, 5);
   });
 
   $scope.orderProp = 'age';
+
+
 });
